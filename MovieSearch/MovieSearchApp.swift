9c7b3b7c6ca7de50545a9d01ee6a419ec4827f85
Danila <danila.fafr@gmail.com>
2025-03-04 17:48:25 +0300

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by Данила Спиридонов on 04.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
