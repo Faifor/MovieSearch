5a662f2a1fb77afe3cc085627d3cd38e49a76f75
Danila <danila.fafr@gmail.com>
2025-03-11 15:04:56 +0300

    detail

---
+++ MovieSearch/MovieView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieSearch
+//
+//  Created by Данила Спиридонов on 04.03.2025.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    @State private var movies: [Movie] = []
+    @State private var currentPage = 1
+    
+    var body: some View
+    
+}
+
+
+
+
+
+
+
+#Preview {
+    MovieView()
+}
