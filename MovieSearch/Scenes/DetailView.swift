57a968823725a2a85521435ce44ae53509d64810
Danila <danila.fafr@gmail.com>
2025-03-13 23:07:00 +0300

    данные грузятся с сервера

--- MovieSearch/Scenes/DetailView.swift
+++ MovieSearch/Scenes/DetailView.swift
@@ -8,49 +8,90 @@
 import SwiftUI
 
 struct DetailView: View {
-    var movie: MovieModel
+    let movie: MovieModel
     
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                if let posterURL = movie.poster?.previewUrl, let url = URL(string: posterURL) {
-                    AsyncImage(url: url) { image in
-                        image.resizable().scaledToFill()
-                    } placeholder: {
-                        Image(systemName: "photo")
-                            .resizable()
-                            .scaledToFit()
+                Group {
+                    if let posterURL = movie.poster?.previewUrl, let url = URL(string: posterURL) {
+                        AsyncImage(url: url) { image in
+                            image.resizable().scaledToFill()
+                        } placeholder: {
+                            Image(systemName: "photo")
+                                .resizable()
+                                .scaledToFill()
+                        }
+                        .frame(height: 250)
                     }
-                    .frame(height: 250)
                 }
+                .padding(.bottom)
+                
+                
+                Spacer().frame(height: 165)
+                
                 Text(movie.name ?? "Без названия")
                     .font(.title)
                     .bold()
                     .padding(.top)
+                    .padding([.leading, .trailing], 16)
+
+                Divider()
                 
-                if let year = movie.year {
-                    Text("Год выпуска: \(year)")
+                Text("Год выпуска: \(movie.year ?? 0)")
                         .font(.subheadline)
                         .padding(.top, 2)
-                }
+                        .padding([.leading, .trailing], 16)
                 
-                Text("Рейтинг: \(movie.rating?.kp ?? 0.0, specifier: "%.1f") (КП)")
+                Divider()
+                
+                Text("Рейтинг: \(movie.rating?.imdb ?? 0.0)")
                     .font(.subheadline)
                     .padding(.top, 2)
+                    .padding([.leading, .trailing], 16)
                 
-                if let description = movie.description {
-                    Text(description)
-                        .padding(.top)
+                Divider()
+                
+                
+            Text(movie.description ?? "Описания нет")
+                        .padding(.top, 2)
                         .padding([.leading, .trailing], 16)
-                } else {
-                    Text("Описание отсутствует")
-                        .padding(.top)
-                        .padding([.leading, .trailing], 16)
-                }
             }
         }
         .navigationTitle(movie.name ?? "Без названия")
     }
 }
+
+
+
+/*#Preview {
+    // Создайте объект `movie` для тестирования, если хотите увидеть отображение
+   DetailView(movie: MovieModel(
+    id: 1,
+    name: "El Atawla",
+    alternativeName: nil,
+    enName: nil,
+    type: nil,
+    typeNumber: nil,
+    year: 2013,
+    description: "Овдовевшая мать, живущая под одной крышей со своей семьёй, незаметно вмешивается в жизни детей и их супругов. Манипулируя ими во имя своей всепоглощающей любви, она провоцирует столкновение между традициями и личными желаниями, что приводит к множеству напряжённых конфликтов.",
+    shortDescription: nil,
+    status: nil,
+    rating: nil,
+    votes: nil,
+    movieLength: nil,
+    totalSeriesLength: nil,
+    seriesLength: nil,
+    ratingMpaa: nil,
+    ageRating: nil,
+    poster: .init(url: "https://image.openmoviedb.com/kinopoisk-images/10893610/e10b13c7-6c31-4a7f-9efe-6c19c67dc5fc/x1000", previewUrl: "https://image.openmoviedb.com/kinopoisk-images/10893610/e10b13c7-6c31-4a7f-9efe-6c19c67dc5fc/orig"),
+    backdrop: nil,
+    genres: nil,
+    countries: nil,
+    releaseYears: nil,
+    isSeries: nil,
+    ticketsOnSale: nil))
+}
+*/
 
 
